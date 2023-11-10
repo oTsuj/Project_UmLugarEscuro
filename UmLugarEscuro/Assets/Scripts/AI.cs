c81fbf715d94a2777bd74b8cbe3d603de2a9c034
Justo Fernandes <justoneto0@hotmail.com>
2023-11-10 19:02:19 -0300

    rec

---
+++ UmLugarEscuro/Assets/Scripts/AI.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AI : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Transform target;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(target.position);
+    }
+}
